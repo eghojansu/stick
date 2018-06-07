bdd452cf7366a0fae802355fb9a8e6a83931935b
Eko Kurniawan <setiawanegho@gmail.com>
2018-06-07 11:27:03 +0700

    Change indent size for template fixture

--- tests/fixture/template/include.php
+++ tests/fixture/template/include.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Main layout</title>
+  <meta charset="UTF-8">
+  <title>Main layout</title>
 </head>
 <body>
-    <p>This view include another view named "includeme" with the following content:</p>
+  <p>This view include another view named "includeme" with the following content:</p>
 
-    <div id="partialcontent"><?= $this->include('includeme', null, 3) ?></div>
+  <div id="partialcontent"><?= $this->include('includeme', null, 3) ?></div>
 
-    <p>This is calling to strtoupper function with upper alias: <?= $this->upper('foo') ?></p>
-    <p>This is calling to strtolower function with lower alias: <?= $this->lower('FOO') ?></p>
-    <p>This is calling to lcfirst function: <?= $this->lcfirst('FOO') ?></p>
+  <p>This is calling to strtoupper function with upper alias: <?= $this->upper('foo') ?></p>
+  <p>This is calling to strtolower function with lower alias: <?= $this->lower('FOO') ?></p>
+  <p>This is calling to lcfirst function: <?= $this->lcfirst('FOO') ?></p>
 </body>
 </html>
--- tests/fixture/template/single.php
+++ tests/fixture/template/single.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title><?= $pageTitle ?></title>
+  <meta charset="UTF-8">
+  <title><?= $pageTitle ?></title>
 </head>
 <body>
   <p>Single page with uppercased <?= $this->upper('title') ?>.</p>
