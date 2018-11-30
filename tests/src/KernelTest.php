fd8cf2dac4ba5c299ba65d61e58b98910d3083be
Eko Kurniawan <setiawanegho@gmail.com>
2018-11-30 23:09:40 +0700

    add kernel class

---
+++ tests/src/KernelTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This file is part of the eghojansu/stick library.
+ *
+ * (c) Eko Kurniawan <ekokurniawanbs@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Created at Nov 30, 2018 22:58
+ */
+
+namespace Fal\Stick\Test;
+
+use Fal\Stick\Kernel;
+use PHPUnit\Framework\TestCase;
+
+class KernelTest extends TestCase
+{
+    private $kernel;
+
+    public function setUp()
+    {
+        $this->kernel = new TestKernel();
+    }
+
+    public function testCreate()
+    {
+        $this->assertNotSame($this->kernel, TestKernel::create());
+    }
+
+    public function testGetFw()
+    {
+        $this->assertInstanceOf('Fal\\Stick\\Fw', $this->kernel->getFw());
+    }
+
+    public function testGetEnvironment()
+    {
+        $this->assertEquals('prod', $this->kernel->getEnvironment());
+    }
+
+    public function testSetEnvironment()
+    {
+        $this->assertEquals('foo', $this->kernel->setEnvironment('foo')->getEnvironment());
+    }
+
+    public function testRun()
+    {
+        $this->expectOutputRegex('/No route defined./');
+
+        $this->kernel->run();
+    }
+}
+
+class TestKernel extends Kernel
+{
+}
