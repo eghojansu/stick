963b51bbb4417a0c3190cf7b54b8bfd5686bff5a
Eko Kurniawan <setiawanegho@gmail.com>
2019-03-03 14:36:45 +0700

    Add apc/apcu cache

---
+++ src/Stick/Cache/Apcu.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * This file is part of the eghojansu/stick library.
+ *
+ * (c) Eko Kurniawan <ekokurniawanbs@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Fal\Stick\Cache;
+
+/**
+ * Apcu cache.
+ *
+ * @author Eko Kurniawan <ekokurniawanbs@gmail.com>
+ */
+class Apcu implements CacheInterface
+{
+    /**
+     * @var string
+     */
+    protected $prefix;
+
+    /**
+     * Class constructor.
+     *
+     * @param string|null $prefix
+     */
+    public function __construct(string $prefix = null)
+    {
+        $this->prefix = $prefix ?? 'stick_cache_';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function exists(string $key): bool
+    {
+        return apcu_exists($this->prefix.$key);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get(string $key): ?CacheItem
+    {
+        $raw = apcu_fetch($this->prefix.$key);
+
+        return $raw ? CacheItem::fromString($raw) : null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function set(string $key, CacheItem $value): bool
+    {
+        return apcu_store($this->prefix.$key, $value->toString(), $value->getTtl());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function clear(string $key): bool
+    {
+        return apcu_delete($this->prefix.$key);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function reset(string $suffix = null): int
+    {
+        $affected = 0;
+        $pattern = '/^'.preg_quote($this->prefix, '/').'.+'.preg_quote($suffix ?? '', '/').'$/';
+
+        foreach (new \APCUIterator($pattern) as $item) {
+            $affected += (int) apcu_delete($item['key']);
+        }
+
+        return $affected;
+    }
+}
---
+++ tests/Stick/Cache/ApcTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * This file is part of the eghojansu/stick library.
+ *
+ * (c) Eko Kurniawan <ekokurniawanbs@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Created at Jan 22, 2019 14:14
+ */
+
+declare(strict_types=1);
+
+namespace Fal\Stick\Test\Cache;
+
+use Fal\Stick\Cache\CacheItem;
+use Fal\Stick\Cache\Apc;
+use PHPUnit\Framework\TestCase;
+
+class ApcTest extends TestCase
+{
+    private $cache;
+
+    public function setup()
+    {
+        $this->cache = new Apc();
+    }
+
+    public function testExists()
+    {
+        $this->assertFalse($this->cache->exists('foo'));
+    }
+
+    public function testGet()
+    {
+        $this->assertNull($this->cache->get('foo'));
+    }
+
+    public function testSet()
+    {
+        $this->assertTrue($this->cache->set('foo', new CacheItem('foo')));
+        $this->assertEquals('foo', $this->cache->get('foo')->getValue());
+        $this->cache->reset();
+    }
+
+    public function testClear()
+    {
+        $this->assertFalse($this->cache->clear('foo'));
+    }
+
+    public function testReset()
+    {
+        $this->assertEquals(0, $this->cache->reset());
+
+        $this->assertTrue($this->cache->set('foo', new CacheItem('foo')));
+        $this->assertTrue($this->cache->set('foo.bar', new CacheItem('foo')));
+        $this->assertTrue($this->cache->set('bar.bar', new CacheItem('foo')));
+        $this->assertEquals(2, $this->cache->reset('.bar'));
+        $this->assertEquals(1, $this->cache->reset());
+    }
+}
